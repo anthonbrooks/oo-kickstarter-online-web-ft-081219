e8978ba1a7d6cc21493b5cc2dc848993f3d982e9
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-09-03 19:41:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,10 @@
 class Project
   
-  attr_accessor :title  
+  attr_reader :title  
   
-  def initialize(title)
+  def initialize(title, backers)
     @title = title
+    @backers = []
   end 
   
 end 
